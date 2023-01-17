11024892dfda57ea8141aaa22282bdd3e9d3f145
Emerson <emerson.day@gmail.com>
2023-01-17 10:51:52 -0800

    added react-modal for deleting an answer

--- src/components/manage/AnswersTable.jsx
+++ src/components/manage/AnswersTable.jsx
@@ -1,9 +1,38 @@
+import Modal from "react-modal";
 import { useState } from "react";
 import answersData from "../../data/answers";
 
+const customStyles = {
+   content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+   },
+};
 function AnswersTable({ selectedQuestion, setSelectedQuestion }) {
-   const [isOpen, setIsOpen] = useState(false);
+   const [modalIsOpen, setIsOpen] = useState(false);
    const [answers, setAnswers] = useState(answersData);
+   const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+   function openModal(answer) {
+      setIsOpen(true);
+      setSelectedAnswer(answer.answerId);
+   }
+
+   function closeModal() {
+      setIsOpen(false);
+   }
+
+   function onDelete() {
+      const newAnswers = answers.filter(
+         (answer) => answer.answerId !== selectedAnswer
+      );
+      setAnswers(newAnswers);
+      closeModal();
+   }
 
    return (
       <div>
@@ -35,13 +64,28 @@ function AnswersTable({ selectedQuestion, setSelectedQuestion }) {
                         </td>
                         <td>
                            <div>
-                              <button>delete</button>
+                              <button onClick={() => openModal(answer)}>
+                                 delete
+                              </button>
                            </div>
                         </td>
                      </tr>
                   ))}
             </tbody>
          </table>
+         <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="delete answer"
+         >
+            <h2>delete answer</h2>
+            <div>are you sure you want to delete</div>
+            <form>
+               <button onClick={closeModal}>cancel</button>
+               <button onClick={onDelete}>delete</button>
+            </form>
+         </Modal>
       </div>
    );
 }
