e58d272fdd137862d7bd20e2740a5f819b49654d
Emerson <emerson.day@gmail.com>
2023-01-25 23:48:13 -0800

    created username input component

--- src/components/create/CreatePage.jsx
+++ src/components/create/CreatePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Username from "./Username";
+import styles from "./create.module.css";
 function CreatePage() {
-   return <div>CreatePage</div>;
+   return (
+      <div className={styles.createPage}>
+         <h2>create a new game</h2>
+
+         <Username />
+      </div>
+   );
 }
 
 export default CreatePage;
