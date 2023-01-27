7331834eea3d5105cfb585ac13c0954a141cba50
Emerson <emerson.day@gmail.com>
2023-01-27 11:19:38 -0800

    create game in firestore

--- src/components/create/CreatePage.jsx
+++ src/components/create/CreatePage.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import Username from "./Username";
 import Lobby from "./Lobby";
 import styles from "./create.module.css";
+import { useCollection } from "../../hooks/useCollection";
+
 function CreatePage() {
    const [username, setUsername] = React.useState("");
+   const { documents: categories } = useCollection("categories");
    return (
       <div className={styles.createPage}>
          <h2>create a new game</h2>
 
          {!username ? (
-            <Username username={username} setUsername={setUsername} />
+            <Username
+               username={username}
+               setUsername={setUsername}
+               categories={categories}
+            />
          ) : (
-            <Lobby username={username} setUsername={setUsername} />
+            <Lobby
+               username={username}
+               setUsername={setUsername}
+               categories={categories}
+            />
          )}
       </div>
    );
--- src/components/create/Username.jsx
+++ src/components/create/Username.jsx
@@ -1,13 +1,37 @@
 import { useRef } from "react";
 import styles from "./create.module.css";
-export default function Username({ username, setUsername }) {
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase/client";
+
+function makeid(length) {
+   let result = "";
+   const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+   const charactersLength = characters.length;
+   let counter = 0;
+   while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+   }
+   return result;
+}
+
+export default function Username({ username, setUsername, categories }) {
    const inputRef = useRef();
-   const onContinue = () => {
+   const onContinue = async () => {
       if (inputRef.current.value.length < 2) {
          alert("username must be at least 2 characters long");
          return;
       }
       setUsername(inputRef.current.value);
+
+      const gameRef = await addDoc(collection(db, "games"), {
+         roomCode: makeid(4),
+         usernames: [inputRef.current.value],
+         category: categories.filter((c) => c.questionCount > 0)[0].id,
+         creator: inputRef.current.value,
+         dateCreated: new Date().toLocaleDateString(),
+      });
    };
 
    return (
