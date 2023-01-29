b412d452e50cfd64cd5c244078f627ed3704840b
Emerson <emerson.day@gmail.com>
2023-01-28 21:24:13 -0800

    created join game page

---
+++ src/components/join/Join.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Join() {
+   return (
+      <div>
+         <h1>enter room code here</h1>
+         <p>enter the room code given to you by the game creator (host)</p>
+         <h3>room code</h3>
+         <input type="text" />
+         <button>continue</button>
+      </div>
+   );
+}
+
+export default Join;
--- src/components/join/JoinPage.jsx
+++ src/components/join/JoinPage.jsx
@@ -1,10 +1,13 @@
 import Username from "./Username";
+import Join from "./Join";
+import styles from "./join.module.css";
 
 export default function JoinPage({ categories }) {
    return (
-      <div>
+      <div className={styles.joinPage}>
          <h2>join a game</h2>
          <Username />
+         <Join />
       </div>
    );
 }
